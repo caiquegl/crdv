41aec6393f11865c577e9517aafccd85a187b760
caiquegl <arq.caique@hotmail.com>
2021-02-15 18:50:41 -0300

    procfile

--- src/View/html/index.php
+++ src/View/html/index.php
@@ -14,7 +14,8 @@ use CRDV\Model\ProductFunctions\ProductDAOMySQL;
 include '../partial/header.php';
 
 ?>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/newStyle.css">
 
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
